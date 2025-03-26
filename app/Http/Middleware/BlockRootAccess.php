a1e16592475ef95bc42cf2fb4e492fff646c7ad0
Dreamless2 <tiagonet50@gmail.com>
2025-03-26 19:21:45 -0300

    Update BlockRootAccess.php

--- app/Http/Middleware/BlockRootAccess.php
+++ app/Http/Middleware/BlockRootAccess.php
@@ -15,6 +15,12 @@ class BlockRootAccess
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if ($request->is('/')) {
+            return response()->json([
+                'error' => 'Acesso negado',
+                'message' => 'Você não tem permissão para acessar esta rota.'
+            ], 403);
+        }
         return $next($request);
     }
 }
