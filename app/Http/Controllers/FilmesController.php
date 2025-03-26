4473224ce61d5372c7ad6f3ee3e95da4f9e57b7e
Dreamless2 <tiagonet50@gmail.com>
2025-03-26 19:32:41 -0300

    Update app.php

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -17,4 +17,45 @@ class FilmesController extends Controller
         $filme = FilmesModel::latest()->first();
         return response()->json($filme);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'codigo' => 'required|numeric',
+            'titulo' => 'required|string',
+            'audio' => ['required', new Enum(AudioEnum::class)],
+            'sinopse' => 'required|string',
+            'titulo_original' => 'required|string',
+            'data_lancamento' => 'required|string',
+            'titulo_alternativo' => 'string',
+            'filme' => 'required|string',
+            'franquia' => 'string',
+            'genero' => 'required|string',
+            'tags' => 'required|string',
+            'diretor' => 'required|string',
+            'estrelas' => 'required|string',
+            'estudio' => 'required|string',
+            'mcu' => 'string'
+        ]);
+
+        $item = FilmesModel::create([
+            'codigo' => $request->codigo,
+            'titulo' => $request->titulo,
+            'audio' => $request->audio,
+            'sinopse' => $request->sinopse,
+            'titulo_original' => $request->titulo_original,
+            'data_lancamento' => $request->data_lancamento,
+            'titulo_alternativo' => $request->titulo_alternativo,
+            'filme' => $request->filme,
+            'franquia' => $request->franquia,
+            'genero' => $request->genero,
+            'tags' => $request->tags,
+            'diretor' => $request->diretor,
+            'estrelas' => $request->estrelas,
+            'estudio' => $request->estudio,
+            'mcu' => $request->mcu,
+        ]);
+
+        return response()->json($item);
+    }
 }
