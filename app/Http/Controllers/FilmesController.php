afee9840c5cb654ea394964aa3530d42623e6054
Dreamless2 <tiagonet50@gmail.com>
2025-03-26 17:22:39 -0300

    Update FilmesModel.php

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class FilmesController extends Controller
 {
-    //
+    public function Primeiro()
+    {
+        $filme = FilmesModel::first();
+        return response()->json($filme);
+    }
+    public function Ultimo()
+    {
+        $filme = FilmesModel::latest()->first();
+        return response()->json($filme);
+    }
+
 }
