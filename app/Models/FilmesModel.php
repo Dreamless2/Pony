83291f339cdff38b38f95e87a4530ebe0bec15b0
Dreamless2 <tiagonet50@gmail.com>
2025-03-26 17:22:20 -0300

    Update FilmesModel.php

--- app/Models/FilmesModel.php
+++ app/Models/FilmesModel.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class FilmesModel extends Model
 {
-    //
+    protected $table = 'filmes';
+    protected $hidden = ['id', 'created_at', 'updated_at'];
+    protected $fillable = ['codigo', 'titulo', 'audio', 'sinopse', 'titulo_original', 'data_lancamento', 'titulo_alternativo', 'filme', 'franquia', 'genero', 'tags', 'diretor', 'estrelas', 'estudio','mcu'];
 }
