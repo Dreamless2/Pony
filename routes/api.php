12b692fba7f5feb844f3a5d079e18fd471ffec3d
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 22:53:14 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilmesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,4 @@ Route::get('/user', function (Request $request) {
 Route::post('/inserir', [FilmesController::class, 'store']);
 
 Route::get('/primeiro', [FilmesController::class, 'Primeiro'])->name('primeiro');
-Route::get('/ultimo',  [FilmesController::class, 'Ultimo'])->name('ultimo');
+Route::get('/ultimo',  [FilmesControllersController::class, 'Ultimo'])->name('ultimo');
