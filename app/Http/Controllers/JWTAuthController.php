4c99d94d773d2ecbfa2b49cac44882ebf07473ce
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:39:28 -0300

    Update JWTAuthController.php

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JWTAuthController extends Controller
