dfb4d09a6f43428dc2d02702971135054127d9a8
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:18:38 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,4 @@ use App\Http\Controllers\FilmesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-
+Route::controller(FilmesController::class)->group(function () {
