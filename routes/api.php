e41881fd0855d1de8289b1f8433d65f38895585b
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:19:19 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // middleware auth sanctum
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
