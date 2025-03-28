73f83ee4f2f19bdcb1b64f570c5873adda7d6778
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:09:46 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\FilmesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')
-
+Route::middleware('auth:sanctum')
 Route::post('/inserir', [FilmesController::class, 'store']);
 Route::get('/primeiro', [FilmesController::class, 'Primeiro'])->name('primeiro');
 Route::get('/ultimo',  [FilmesController::class, 'Ultimo'])->name('ultimo');
