f3177e656f30371bc5a8287a2b902fec0691e8a4
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 22:15:12 -0300

    Update index.blade.php

--- resources/views/Filmes/index.blade.php
+++ resources/views/Filmes/index.blade.php
@@ -30,7 +30,7 @@
                 <div class="row g-2">
                     <div class="col-md-6">
                         <label class="form-label">Título Original</label>
-                        <input type="text" class="form-control" placeholder="Título Original">
+                        <input type="text" class="form-control" placeholder="Título Original" value="{{ $filme->getTitleOriginal() }}">
                     </div>
                     <div class="col-md-6">
                         <label class="form-label">Título Alternativo</label>
