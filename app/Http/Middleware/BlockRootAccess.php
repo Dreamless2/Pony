3ce6d67a6474ff0e80eb3ed21b0037f4ccfd03db
Dreamless2 <tiagonet50@gmail.com>
2025-03-28 10:11:59 -0300

    Update sanctum.php

--- app/Http/Middleware/BlockRootAccess.php
+++ app/Http/Middleware/BlockRootAccess.php
@@ -16,7 +16,7 @@ class BlockRootAccess
     public function handle(Request $request, Closure $next): Response
     {
         if ($request->path() === '/') {
-            return response('Você não tem permissão para acessar esta rota.', 403)->header('Content-Type', 'text/plain');
+            return response('Você não tem permissão para acessar este recurso.', 403)->header('Content-Type', 'text/plain');
         }
         return $next($request);
     }
