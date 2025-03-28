d687c3a035be975e6491915528a7e1739d3cf873
Dreamless2 <tiagonet50@gmail.com>
2025-03-28 10:55:18 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
