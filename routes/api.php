71ff6e8c0bc3185c2c3a11c665528a8ffcac9315
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:20:27 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,7 @@ use Illuminate\Support\Facades\Route;
 // middleware auth sanctum filmes controller
 Route::middleware('auth:sanctum')->group(function () {
     Route::controller(FilmesController::class)->group(function () {
-        Route::get('/filmes', 'index');
-        Route::get('/filmes/{id}', 'show');
-        Route::post('/filmes', 'store');
-        Route::put('/filmes/{id}', 'update');
-        Route::delete('/filmes/{id}', 'destroy');
+        Route::get('/primeoro', 'primeiro');
     });
 });
 });
