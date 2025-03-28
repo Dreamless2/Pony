84a669b23f4ded3216afd83588f69e095c89277c
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 22:36:01 -0300

    Update FilmesModel.php

--- app/Models/FilmesModel.php
+++ app/Models/FilmesModel.php
@@ -8,6 +8,6 @@ class FilmesModel extends Model
 {
     protected $table = 'filmes';
     protected $primaryKey = 'id';
-    protected $hidden = ['id', 'created_at', 'updated_at'];
+    protected $hidden = ['created_at', 'updated_at'];
     protected $fillable = ['codigo', 'titulo', 'audio', 'sinopse', 'titulo_original', 'data_lancamento', 'titulo_alternativo', 'filme', 'franquia', 'genero', 'tags', 'diretor', 'estrelas', 'estudio','mcu'];
 }
