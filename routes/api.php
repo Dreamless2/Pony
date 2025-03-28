e381492487c2c718639c393bf860899ca96762c8
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:19:28 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,8 @@ use App\Http\Controllers\FilmesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// middleware auth sanctum
+// middleware auth sanctum filmes controller
+Route::get('/filmes', [FilmesController::class, 'index']);
+Route::get('/filmes/{id}', [FilmesController::class, 'show']);
+Route::post('/filmes', [FilmesController::class, 'store']); 
 
