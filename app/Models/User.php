d7423348336d7a13190376d659aae699a484f1c1
Dreamless2 <tiagonet50@gmail.com>
2025-03-28 10:55:10 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+{
 
     /**
      * The attributes that are mass assignable.
