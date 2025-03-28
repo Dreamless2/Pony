986fd86ffca838fcfd06b614c615599455ca62ba
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:18:47 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(FilmesController::class)->group(function () {
+    Route::get('/primeiro', 'Primeiro');
+    Route::get('/ultimo', 'Ultimo');
+    Route::post('/store', 'store');
+});
