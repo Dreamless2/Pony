8c619e99915875f53252bcd70e50a852c074a4c3
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:24:13 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\FilmesController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
     Route::controller(FilmesController::class)->group(function () {
         Route::get('/primeiro', 'primeiro');
     });
