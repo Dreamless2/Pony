ba324e9a64ca8568afb8481f11a8173744d93006
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 22:53:05 -0300

    Update FilmesModel.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/inserir', [FilmesController::class, 'store']);
+
+Route::get('/primeiro', [FilmesController::class, 'Primeiro'])->name('primeiro');
+Route::get('/ultimo',  [FilmesController::class, 'Ultimo'])->name('ultimo');
