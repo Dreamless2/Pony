95f4740c79b7e29993ab0a5c282a6df105537e9c
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 23:06:10 -0300

    Update FilmesController.php

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -42,7 +42,7 @@ class FilmesController extends Controller
         // 🚨 Verificação extra antes de inserir
         if (FilmesModel::where('codigo', $request->codigo)->exists()) {
             return response()->json([
-                'message' => 'Erro: Código já cadastrado.'
+                'message' => 'Erro: Filme "  já cadastrado.'
             ], 400);
         }
 
