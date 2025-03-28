c0b94ff178ee0b841cc64bc3591d6eaf9414ed79
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 21:38:59 -0300

    Update FilmesController.php

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -7,7 +7,7 @@ use App\Models\FilmesModel;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Enum;
 use vfalies\tmdb\Item;
-use vfalies\
+
 
 class FilmesController extends Controller
 {
