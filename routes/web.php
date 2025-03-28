84386d1c028feb8e90b1d57a1af025035174a0cb
Dreamless2 <tiagonet50@gmail.com>
2025-03-27 21:59:27 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\FilmesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',
+Route::get('/', [FilmesController::class, 'index']);
 Route::get('/obterfilme', [FilmesController::class, 'ObterFilme']);
 Route::get('/primeiro', [FilmesController::class, 'Primeiro']);
 Route::get('/ultimo',  [FilmesController::class, 'Ultimo']);
